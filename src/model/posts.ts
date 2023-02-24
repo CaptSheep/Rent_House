0ee53ef27c6dceb9380aa4d3c1d7b406b7d29163
CaptSheep <long.trieu.hr@gmail.com>
2023-02-24 09:53:09 +0700

    create basic models

---
+++ src/model/posts.ts
@@ -0,0 +1,30 @@
+import {Entity,PrimaryGeneratedColumn,Column} from "typeorm";
+
+@Entity()
+export class Posts {
+    @PrimaryGeneratedColumn({type: 'int'})
+    public id: number;
+    @Column({type: 'varchar'})
+    public name: string;
+    @Column({type: 'int'})
+    public price: number;
+    @Column({type: 'varchar'})
+    public address: string;
+    @Column({type: 'longtext'})
+    public description: string;
+    @Column({type: 'int'})
+    public categoryId: number;
+    @Column({type: 'int'})
+    public bedroom: number;
+    @Column({type: 'int'})
+    public bathroom: number;
+    @Column({type: 'varchar', default: 'Available'})
+    public status: string;
+    @Column({type: 'int'})
+    public userId: number;
+    @Column({type: 'varchar', })
+    public avatar: string;
+    @Column({type: 'int', default: 0})
+    public star: number;
+
+}
---
+++ src/model/users.ts
@@ -0,0 +1,19 @@
+import {Entity,PrimaryGeneratedColumn,Column} from "typeorm";
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn({type:'int'})
+    public id : number
+    @Column({type: "varchar", default : ''})
+    public fullName : string
+    @Column({type: "varchar", default : ''})
+    public userName : string
+    @Column({type: "varchar", default : ''})
+    public address : string
+    @Column({type: "varchar", default : ''})
+    public phoneNumber : string
+    @Column({type: "varchar", default : ''})
+    public email : string
+    @Column({type: "varchar", default : ''})
+    public password : string
+}
